887e490cb8ed3016443391bc0f52766255417525
William Cotton <williamc@eurekasoft.com>
2025-02-12 07:57:35 -0600

    move synth out of main component class definition

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,6 +2,99 @@
 #include <JuceHeader.h>
 #include "PianoRollComponent.h"
 
+// Synthesizer voice
+class SineWaveSound : public juce::SynthesiserSound
+{
+public:
+    SineWaveSound() {}
+    bool appliesToNote(int) override { return true; }
+    bool appliesToChannel(int) override { return true; }
+};
+
+class SineWaveVoice : public juce::SynthesiserVoice
+{
+public:
+    SineWaveVoice() {}
+
+    bool canPlaySound(juce::SynthesiserSound* sound) override
+    {
+        return dynamic_cast<SineWaveSound*>(sound) != nullptr;
+    }
+
+    void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound*, int) override
+    {
+        currentAngle = 0.0;
+        level = velocity * 0.15;
+        tailOff = 0.0;
+
+        auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+        auto cyclesPerSample = cyclesPerSecond / getSampleRate();
+        angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
+    }
+
+    void stopNote(float velocity, bool allowTailOff) override
+    {
+        if (allowTailOff)
+        {
+            if (tailOff == 0.0)
+                tailOff = 1.0;
+        }
+        else
+        {
+            clearCurrentNote();
+            angleDelta = 0.0;
+        }
+    }
+
+    void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override
+    {
+        if (angleDelta != 0.0)
+        {
+            if (tailOff > 0.0)
+            {
+                while (--numSamples >= 0)
+                {
+                    auto currentSample = (float)(std::sin(currentAngle) * level * tailOff);
+                    
+                    for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+                        outputBuffer.addSample(i, startSample, currentSample);
+
+                    currentAngle += angleDelta;
+                    ++startSample;
+
+                    tailOff *= 0.99;
+
+                    if (tailOff <= 0.005)
+                    {
+                        clearCurrentNote();
+                        angleDelta = 0.0;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                while (--numSamples >= 0)
+                {
+                    auto currentSample = (float)(std::sin(currentAngle) * level);
+
+                    for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+                        outputBuffer.addSample(i, startSample, currentSample);
+
+                    currentAngle += angleDelta;
+                    ++startSample;
+                }
+            }
+        }
+    }
+
+    void pitchWheelMoved(int) override {}
+    void controllerMoved(int, int) override {}
+
+private:
+    double currentAngle = 0.0, angleDelta = 0.0, level = 0.0, tailOff = 0.0;
+};
+
 class MainComponent : public juce::Component,
                      public juce::Timer,
                      public juce::AudioSource,
@@ -94,99 +187,6 @@ private:
     juce::MidiMessageSequence midiSequence;
     juce::Synthesiser synth;
 
-    // Synthesizer voice
-    class SineWaveSound : public juce::SynthesiserSound
-    {
-    public:
-        SineWaveSound() {}
-        bool appliesToNote(int) override { return true; }
-        bool appliesToChannel(int) override { return true; }
-    };
-
-    class SineWaveVoice : public juce::SynthesiserVoice
-    {
-    public:
-        SineWaveVoice() {}
-
-        bool canPlaySound(juce::SynthesiserSound* sound) override
-        {
-            return dynamic_cast<SineWaveSound*>(sound) != nullptr;
-        }
-
-        void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound*, int) override
-        {
-            currentAngle = 0.0;
-            level = velocity * 0.15;
-            tailOff = 0.0;
-
-            auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-            auto cyclesPerSample = cyclesPerSecond / getSampleRate();
-            angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
-        }
-
-        void stopNote(float velocity, bool allowTailOff) override
-        {
-            if (allowTailOff)
-            {
-                if (tailOff == 0.0)
-                    tailOff = 1.0;
-            }
-            else
-            {
-                clearCurrentNote();
-                angleDelta = 0.0;
-            }
-        }
-
-        void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override
-        {
-            if (angleDelta != 0.0)
-            {
-                if (tailOff > 0.0)
-                {
-                    while (--numSamples >= 0)
-                    {
-                        auto currentSample = (float)(std::sin(currentAngle) * level * tailOff);
-                        
-                        for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-                            outputBuffer.addSample(i, startSample, currentSample);
-
-                        currentAngle += angleDelta;
-                        ++startSample;
-
-                        tailOff *= 0.99;
-
-                        if (tailOff <= 0.005)
-                        {
-                            clearCurrentNote();
-                            angleDelta = 0.0;
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    while (--numSamples >= 0)
-                    {
-                        auto currentSample = (float)(std::sin(currentAngle) * level);
-
-                        for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-                            outputBuffer.addSample(i, startSample, currentSample);
-
-                        currentAngle += angleDelta;
-                        ++startSample;
-                    }
-                }
-            }
-        }
-
-        void pitchWheelMoved(int) override {}
-        void controllerMoved(int, int) override {}
-
-    private:
-        double currentAngle = 0.0, angleDelta = 0.0, level = 0.0, tailOff = 0.0;
-    };
-
     // Playback state
     bool isPlaying = false;
     int currentEvent = 0;
@@ -200,7 +200,6 @@ private:
     double loopEndBeat;
     int loopCount;
     bool isLooping;
-    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
